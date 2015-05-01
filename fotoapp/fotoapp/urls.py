a7ba0bca5536cc37cfdd1a43bf79e3b677920856
Ramiro Motteta <ramox40@gmail.com>
2015-05-01 02:10:44 -0300

    Correccion en urls.py
    Corregido import de static en urls.py principalde fotoapp

--- fotoapp/fotoapp/urls.py
+++ fotoapp/fotoapp/urls.py
@@ -1,4 +1,4 @@
-from conf.urls.static import static
+from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from fotoapp import settings
