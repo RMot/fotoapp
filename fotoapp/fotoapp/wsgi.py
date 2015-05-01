4526841488cc1149be983b2e94be9ecb910b8642
Ramiro Motteta <ramox40@gmail.com>
2015-05-01 01:22:44 -0300

    Prueba del wsgi

--- fotoapp/fotoapp/wsgi.py
+++ fotoapp/fotoapp/wsgi.py
@@ -6,9 +6,45 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+#
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fotoapp.settings")
+#
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
+#!/usr/bin/python
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fotoapp.fotoapp.settings")
+import sys
+
+PROJECT_NAME = "fotoapp.fotoapp"
+
+os.environ['DJANGO_SETTINGS_MODULE'] = PROJECT_NAME + '.settings'
+#sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', PROJECT_NAME))
+
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.environ['PROJECT_HOME'])
+virtenv = os.environ['WORKON_HOME'] + '/development/'
+
+
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append(os.path.join(virtenv, 'lib/python2.7/site-packages'))
+
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+import django
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+#from plataforma.plataforma.wsgi import baseapp
--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,10 @@ PROJECT_NAME = "fotoapp.fotoapp"
 os.environ['DJANGO_SETTINGS_MODULE'] = PROJECT_NAME + '.settings'
 #sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', PROJECT_NAME))
 
+sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.environ['PROJECT_HOME'])
 virtenv = os.environ['WORKON_HOME'] + '/development/'
+
 
 os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
@@ -24,10 +26,11 @@ sys.path.append(os.path.join(virtenv, 'lib/python2.7/site-packages'))
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-import django.core.handlers.wsgi
 import django
 django.setup()
-application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 #from plataforma.plataforma.wsgi import baseapp
 
